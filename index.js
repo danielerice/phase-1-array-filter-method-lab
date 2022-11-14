42548fdf43876a4db48aa88f7e38482f4c617641
Daniel Rice <danielrice5627@gmail.com>
2022-11-14 11:35:38 -0800

    commit tree ebce35417957863f18e49d8e8001274427feb0d8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(function(arrayName) {
+        return arrayName.toUpperCase()===string.toUpperCase();
+    })
+}
+function fuzzyMatch(array, string) {
+    return array.filter(function(arrayName) {
+        return arrayName.startsWith(string);
+    })
+}
+function matchName(array, string) {
+    return array.filter(function(arrayName) {
+        return arrayName.name===string;
+    })
+}
